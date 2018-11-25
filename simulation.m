88e11b4a186bb8f2e0053014ac04e8f406d0b4c1
jakethakur <34597857+jakethakur@users.noreply.github.com>
2018-11-25 15:57:59 +0000

    simulation finds correct values

--- simulation.m
+++ simulation.m
@@ -1,78 +1,63 @@
 %
-% SIMULATION VARIABLES
-%
-
-simulationSpeed = 1; % speed of objects is multiplied by this (CURRENTLY DOES NOTHING)
-simulationLength = 1000; % number of iterations simulation should be run for
-
-%
-% SETUP
-%
-
-runnerPositionX = zeros(simulationLength, 1);
-runnerPositionY = zeros(simulationLength, 1);
-
-chaserPositionX = zeros(simulationLength, 1);
-chaserPositionY = zeros(simulationLength, 1);
-
-collision = false; % if there has been a collision
-collisionX;
-collisionY;
-
-%
 % USER-EDIT VARIABLES
 %
 
 % runner start position
-runnerPositionX(1, 1) = 100;
-runnerPositionY(1, 1) = 100;
+runnerPosition(1) = 100;
+runnerPosition(2) = 100;
 
 % runner velocity
 runnerSpeed = 1;
 runnerDirection = deg2rad(100); % angle is converted to radians
 
 % chaser start position
-chaserPositionX(1, 1) = 200;
-chaserPositionY(1, 1) = 200;
+chaserPosition(1) = 50;
+chaserPosition(2) = 50;
 
 % chaser speed
 chaserSpeed = 2;
 
 %
-% DO NOT EDIT THESE VARIABLES
-%
-
-% chaser direction
-chaserDirection = deg2rad(200); % angle is converted to radians
-
-%
 % CALCULATIONS
 %
 
-% iterate for simulation length
-for i = 2:simulationLength+1
-   % move runner
-   runnerPositionX(i, 1) = runnerPositionX(i-1, 1) + cos(runnerDirection) * runnerSpeed;
-   runnerPositionY(i, 1) = runnerPositionY(i-1, 1) + sin(runnerDirection) * runnerSpeed;
-   
-   % move chaser
-   chaserPositionX(i, 1) = chaserPositionX(i-1, 1) + cos(chaserDirection) * chaserSpeed;
-   chaserPositionY(i, 1) = chaserPositionY(i-1, 1) + sin(chaserDirection) * chaserSpeed;
-   
-   % check for collision
-   if chaserPositionX(i, 1) == runnerPositionX(i, 1) && chaserPositionY(i, 1) == runnerPositionY(i, 1)
-       collision = true;
-       collisionX = chaserPositionX(i, 1);
-       collisionY = chaserPositionY(i, 1);
-   end
+distanceVector = [chaserPosition(1) - runnerPosition(1), chaserPosition(2) - runnerPosition(2)];
+distance = norm(distanceVector); % magnitude (distance scalar value)
+
+runnerVelocity = [runnerSpeed * cos(runnerDirection), runnerSpeed * sin(runnerDirection)];
+
+% find time of collision
+[timeUntilCollision1, timeUntilCollision2] = solveQuadratic(chaserSpeed^2 - runnerSpeed^2, 2*(dot(runnerVelocity, distanceVector)), -(distance^2));
+
+% find points of collision
+
+% collision point 1
+collision1Position(1) = NaN;
+collision1Position(2) = NaN;
+if ~isnan(timeUntilCollision1) && timeUntilCollision1 > 0
+    collision1Position = runnerPosition + runnerVelocity*timeUntilCollision1;
 end
 
-%
-% PLOT
-%
+% collision point 2
+collision2Position(1) = NaN;
+collision2Position(2) = NaN;
+if ~isnan(timeUntilCollision2) && timeUntilCollision2 > 0
+    collision2Position = runnerPosition + runnerVelocity*timeUntilCollision2;
+end
 
-figure;
-plot(runnerPositionX, runnerPositionY, chaserPositionX, chaserPositionY);
+% chaser velocity
+chaserVelocity = NaN;
+% base off of a valid collision
+if ~isnan(collision1Position(1))
+    chaserVelocity = (collision1Position - chaserPosition) / timeUntilCollision1;
+elseif ~isnan(collision2Position(1))
+    chaserVelocity = (collision2Position - chaserPosition) / timeUntilCollision2;
+end
+
+% chaser direction
+if ~isnan(chaserVelocity(1))
+    chaserDirection = atan(chaserVelocity(2)/chaserVelocity(1));
+end
 
 %
 % ANIMATION
@@ -80,3 +65,25 @@ plot(runnerPositionX, runnerPositionY, chaserPositionX, chaserPositionY);
 
 % https://uk.mathworks.com/help/matlab/creating_plots/trace-marker-along-line.html
 % https://uk.mathworks.com/help/matlab/creating_plots/move-group-of-objects-along-line.html
+
+%
+% FUNCTIONS
+%
+function [root1, root2] = solveQuadratic(a, b, c)
+
+  d = b^2 - 4*a*c; % your number under the root sign in quad. formula
+
+  % real numbered distinct roots?
+  if d > 0
+    root1 = (-b+sqrt(d))/(2*a);
+    root2 = (-b-sqrt(d))/(2*a);
+  % real numbered degenerate root?
+  elseif d == 0 
+    root1 = -b/(2*a);
+    root2 = NaN;
+  % complex roots, return NaN, NaN
+  else
+    root1 = NaN;
+    root2 = NaN;
+  end    
+end
